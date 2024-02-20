63206447c8c5a4b25dc5a030da26a0485e6b966e
JayDemitri <jaydemitri24@gmail.com>
2024-02-20 10:17:28 +0000

    Added deps, updated readme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"repairpad-playground","appId":"1:934968069815:web:10d0415943fc83de5fc732","storageBucket":"repairpad-playground.appspot.com","apiKey":"AIzaSyBZI8qGIPYuDnrwF8CW_5rFyZZ0Ah_vAys","authDomain":"repairpad-playground.firebaseapp.com","messagingSenderId":"934968069815","measurementId":"G-J7L8J8EL9F"}))), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService, importProvidersFrom(provideFirestore(() => getFirestore()))]
 };

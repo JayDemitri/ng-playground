e9e582168c92ae686058cc9978235a0e6d45cdb0
JayDemitri <jaydemitri24@gmail.com>
2024-03-01 14:11:55 +0000

    Added layouts

---
+++ src/app/layout/centered/centered.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'ngp-centered',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './centered.component.html',
+  styleUrl: './centered.component.scss'
+})
+export class CenteredComponent {
+
+}
---
+++ src/app/layout/standard/standard.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { ThemeManagerService } from '../../shared/services/theme-manager.service';
+import { BrowserStorageService } from '../../shared/services/browser-storage.service';
+
+@Component({
+  selector: 'ngp-standard',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule
+  ],
+  templateUrl: './standard.component.html',
+  styleUrl: './standard.component.scss'
+})
+export class StandardComponent {
+    themeManager = inject(ThemeManagerService);
+    bs = inject(BrowserStorageService)
+
+    clearStore(){
+      this.bs.clear()
+    }
+}

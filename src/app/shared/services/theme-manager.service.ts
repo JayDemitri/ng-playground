2aae3c64ee68693f51dd404a5ac994e4a3100fc7
JayDemitri <jaydemitri24@gmail.com>
2024-03-02 11:49:15 +0000

    Added shared services

---
+++ src/app/shared/services/theme-manager.service.ts
@@ -0,0 +1,25 @@
+import { DOCUMENT } from '@angular/common';
+import { Injectable, inject } from '@angular/core';
+import { BrowserStorageService } from './browser-storage.service';
+import { BehaviorSubject } from 'rxjs';
+
+const LOCAL_STORAGE_KEY = 'NGP'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeManagerService {
+
+  // Our document instance
+  private document = inject(DOCUMENT)
+  // Browser storage service
+  private bStore = inject(BrowserStorageService)
+
+  constructor() {
+
+    console.log('Theme Manager Started');
+    this.bStore.set('ngp-msg', 'Ng Playground Message')
+    console.log(this.bStore.get('ngp-msg'))
+  }
+
+}
